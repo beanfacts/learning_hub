588bf3587087d2e5aafd9f6483f5e069f3b2eccb
poiler22 <bright_ra2@hotmail.com>
2021-05-24 21:45:38 +0700

    Update ac_iotcore.ino
    Fix multiple publish topic to publish only one.

--- ac_iotcore.ino
+++ ac_iotcore.ino
@@ -14,8 +14,6 @@
 
 int msgReceived = 0;
 String rcvdPayload;
-
-
 const uint16_t IrPin = 4; 
 //int temp = 25;
 IRsend irsend(IrPin);  
@@ -26,33 +24,13 @@ MQTTClient client = MQTTClient(512);
 
 void updateTemp(int target_temp){
   irpanasonic.setTemp(target_temp);
-  irpanasonic.send(1);
-  StaticJsonDocument<200> doc;
-  char jsonBuffer[200];
-  doc["state"]["reported"]["temp"] = target_temp;
-  serializeJson(doc, jsonBuffer);   
-  Serial.println("Publish new temperature value");
-  Serial.println(jsonBuffer);
-  client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
   delay(10);
 }
 
 
 void turnOn(){
-  Serial.println("turn on");
-  Serial.println(irpanasonic.getTemp());
-  irpanasonic.begin();
-  irpanasonic.stateReset();
   irpanasonic.on();
-  irpanasonic.send(2);
   Serial.println("turn on end");
-   StaticJsonDocument<200> doc;
-  char jsonBuffer[200];
-  doc["state"]["reported"]["state"] = 1;
-  serializeJson(doc, jsonBuffer);   
-  Serial.println("Publish state = on value");
-  Serial.println(jsonBuffer);
-  client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
   delay(10);
 }
 
@@ -61,54 +39,32 @@ void updateFan(int level){
        Serial.println(irpanasonic.toString());
       Serial.println("fan1");
       irpanasonic.setFan(0);
-      irpanasonic.send(1);
      }
      else if (level == 2) {
       irpanasonic.setFan(2);
-      irpanasonic.send(1);
      }
      else if (level == 3) {
       irpanasonic.setFan(4);
-      irpanasonic.send(1);
      }
      else if (level == 0){
       irpanasonic.setFan(7);
-      irpanasonic.send(1);
      }
-    StaticJsonDocument<200> doc;
-    char jsonBuffer[200];
-    doc["state"]["reported"]["fan"] = level;
-    serializeJson(doc, jsonBuffer);   
-    Serial.println("Publish new fan level value");
-    Serial.println(jsonBuffer);
-    client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
     delay(10);
 }
 void updateSwing(const char* swing_ac){
      if (!strcmp("auto",swing_ac)){
       Serial.println("Auto");
       irpanasonic.setSwingVertical(0xF);
-      irpanasonic.send(1);
      }
      else if (!strcmp("up",swing_ac)) {
       irpanasonic.setSwingVertical(0x1);
-      irpanasonic.send(1);
      }
      else if (!strcmp("middle",swing_ac)) {
       irpanasonic.setSwingVertical(0x3);
-      irpanasonic.send(1);
      }
      else if (!strcmp("down",swing_ac)){
       irpanasonic.setSwingVertical(0x5);
-      irpanasonic.send(1);
      }
-    StaticJsonDocument<200> doc;
-    char jsonBuffer[200];
-    doc["state"]["reported"]["swing"] = swing_ac;
-    serializeJson(doc, jsonBuffer);   
-    Serial.println("Publish new swing level value");
-    Serial.println(jsonBuffer);
-    client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
      delay(10);
 }
 void updateMode(const char*  mode_ac){
@@ -117,48 +73,24 @@ void updateMode(const char*  mode_ac){
       if (!strcmp(mode_ac,"auto")){
       Serial.println("Auto");
       irpanasonic.setMode(0);
-      irpanasonic.send(1);
      }
      else if (!strcmp(mode_ac,"cool")) {
       Serial.println("here");
       irpanasonic.setMode(3);
-      irpanasonic.send(1);
      }
      else if (!strcmp(mode_ac,"heat")) {
       irpanasonic.setMode(4);
-      irpanasonic.send(1);
      }
      else if (!strcmp(mode_ac,"dry")){
       irpanasonic.setMode(2);
-      irpanasonic.send(1);
      }
      else if (!strcmp(mode_ac,"fan")){
       irpanasonic.setMode(6);
-      irpanasonic.send(1);
      }   
-    char status_str[300];
-    irpanasonic.toString().toCharArray(status_str,300);
-    Serial.println(status_str);
-     StaticJsonDocument<200> doc;
-    char jsonBuffer[200];
-    doc["state"]["reported"]["mode"] = mode_ac;
-    serializeJson(doc, jsonBuffer);   
-    Serial.println("Publish new mode value");
-    Serial.println(jsonBuffer);
-    client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
      delay(10);
 }
 void turnOff(){
-  irpanasonic.stateReset();
   irpanasonic.off();
-  irpanasonic.send(1);
-  StaticJsonDocument<200> doc;
-  char jsonBuffer[200];
-  doc["state"]["reported"]["state"] = 0;
-  serializeJson(doc, jsonBuffer);   
-  Serial.println("Publish state = off value");
-  Serial.println(jsonBuffer);
-  client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
   delay(10);
   
 }
@@ -222,11 +154,11 @@ void setup() {
   connectAWS(); 
   StaticJsonDocument<200> doc;
   char jsonBuffer[200];
-  doc["state"]["reported"]["state"] = NULL;
-  doc["state"]["reported"]["temp"] = NULL;
-  doc["state"]["reported"]["fan"] = NULL;
-  doc["state"]["reported"]["swing"] = NULL;
-  doc["state"]["reported"]["mode"] = NULL;
+  doc["state"]["reported"]["state"] = (char*)0;
+  doc["state"]["reported"]["temp"] = (char*)0;
+  doc["state"]["reported"]["fan"] = (char*)0;
+  doc["state"]["reported"]["swing"] = (char*)0;
+  doc["state"]["reported"]["mode"] = (char*)0;
   serializeJson(doc, jsonBuffer);   
   Serial.println("Publish NUll to initialized shadow");
   Serial.println(jsonBuffer);
@@ -238,7 +170,8 @@ void loop() {
 
    if(msgReceived == 1)
     {
-        Serial.println(msgReceived);
+        StaticJsonDocument<200> doc;
+        char jsonBuffer[200];
         delay(100);
         msgReceived = 0;
         Serial.print("Received Message:");
@@ -251,48 +184,52 @@ void loop() {
            return;
         }
         JsonObject root = sensor_doc["state"].as<JsonObject>();
-        int temp_ac = -1;
-        int fan_ac = -1;
-        int state_ac = -1;
-        char const* mode_ac = "";
-        char const* swing_ac = "";
+        char const* mode_ac = (char*)0;
+        char const* swing_ac = (char*)0;
         delay(100);
         if (root.containsKey("temp")) {
-          temp_ac = sensor_doc["state"]["temp"];
+          int temp_ac = sensor_doc["state"]["temp"];
           updateTemp(temp_ac);
+          doc["state"]["reported"]["temp"] = temp_ac;
           Serial.println("temp");
         }
-        if (root.containsKey("mode")) {
+       if (root.containsKey("mode")) {
           mode_ac = sensor_doc["state"]["mode"];
           updateMode(mode_ac);
+          doc["state"]["reported"]["mode"] = mode_ac;
           Serial.println("mode");
         }
-       if (root.containsKey("swing")) {
+      if (root.containsKey("swing")) {
           swing_ac = sensor_doc["state"]["swing"];
           updateSwing(swing_ac);
+          doc["state"]["reported"]["swing"] = swing_ac;
           Serial.println("swing");
         }
       if (root.containsKey("state")) {
-          state_ac = sensor_doc["state"]["state"];
+          int state_ac = sensor_doc["state"]["state"];
           Serial.println("state");
           if (state_ac == 1){
             turnOn();
+            doc["state"]["reported"]["state"] = 1;
           }
           else if (state_ac == 0){
             turnOff();
+            doc["state"]["reported"]["state"] = 0;
           }
         }
       if (root.containsKey("fan")) {
-          fan_ac = sensor_doc["state"]["fan"];
+          int fan_ac = sensor_doc["state"]["fan"];
           updateFan(fan_ac);
+          doc["state"]["reported"]["fan"] = fan_ac;
           Serial.println("fan");
         }
-      Serial.println(temp_ac);
-      Serial.println(mode_ac);
-      Serial.println(swing_ac);
-      Serial.println(state_ac);
-      Serial.println(fan_ac);
-      Serial.println("AAAA##############################################");
+      irpanasonic.send(2);
+      serializeJson(doc, jsonBuffer);   
+      Serial.println("Publish report information to shadow");
+      Serial.println(jsonBuffer);
+      client.publish(AWS_IOT_PUBLISH_TOPIC,jsonBuffer);
+      Serial.println(msgReceived);
+      Serial.println("##############################################");
     }
   client.loop();
   delay(10);
