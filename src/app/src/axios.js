ea323f3fb3a03543774c4b830270a55d65f7ffdd
Lucky <vorachat1239@gmail.com>
2021-05-30 21:29:47 +0700

    update packages and cleanup

--- src/app/src/axios.js
+++ src/app/src/axios.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   //TODO change the randomuser to actual API
-  baseURL: "https://iff6r2m3p6.execute-api.ap-southeast-1.amazonaws.com/api/v1/",
+  baseURL:
+    "https://iff6r2m3p6.execute-api.ap-southeast-1.amazonaws.com/api/v1/",
+  // baseURL: "http://127.0.0.1:5000/api/v1/",
 });
 
 export default instance;
